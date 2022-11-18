6d299660689065e3cdd2455de13291748a9744d5
LukaszPietrzykowski16 <lukaszpietrzykowsk@gmail.com>
2022-11-18 16:36:57 +0100

    friday commit

--- src/app/icons/icons.module.ts
+++ src/app/icons/icons.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 
 import { FeatherModule } from 'angular-feather';
-import { ShoppingCart, Facebook, Instagram, Youtube } from 'angular-feather/icons';
+import { ShoppingCart, Facebook, Instagram, Youtube, Trash2 } from 'angular-feather/icons';
 
 // Select some icons (use an object, not an array)
 const icons = {
   ShoppingCart, 
   Facebook, 
   Instagram, 
-  Youtube
+  Youtube, 
+  Trash2
 };
 
 @NgModule({
