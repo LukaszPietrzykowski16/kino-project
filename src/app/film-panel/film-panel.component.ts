2823ba67f45d81fce1c22da904890e8ad01356f2
LukaszPietrzykowski16 <lukaszpietrzykowsk@gmail.com>
2022-11-28 16:26:13 +0100

    more button works on first page

--- src/app/film-panel/film-panel.component.ts
+++ src/app/film-panel/film-panel.component.ts
@@ -11,8 +11,20 @@ import { Moment } from 'moment';
 export class FilmPanelComponent {
   description:string = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolore quisquam, atque molestias distinctio ab numquam hic ipsa a dolores rerum aliquam nisi autem voluptate minima eaque veritatis ratione voluptatem! Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolore quisquam, atque molestias distinctio ab numquam hic ipsa a dolores rerum aliquam nisi autem voluptate minima eaque veritatis ratione voluptatem!'
 
+  shortDescription = this.description.substring(0,240)
+
+  flag = true
+
   constructor() {}
 
+  more(){
+    if(this.flag === true) {
+      this.flag = false
+    } else {
+      this.flag = true
+    }
+    console.log(this.flag)
+  }
 
   ngOnInit(): void {
     this.description.substring(0,10)
