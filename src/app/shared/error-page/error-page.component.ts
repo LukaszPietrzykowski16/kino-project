ccf97d7580ec5201a85c9f210ed49bfaf219485f
LukaszPietrzykowski16 <lukaszpietrzykowsk@gmail.com>
2023-01-23 09:17:26 +0100

    feat: cleaning up the project

--- src/app/shared/error-page/error-page.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-import { ApiServiceService } from '../../api-service.service';
-@Component({
-  selector: 'app-error-page',
-  templateUrl: './error-page.component.html',
-  styleUrls: ['./error-page.component.css'],
-})
-export class ErrorPageComponent {}
