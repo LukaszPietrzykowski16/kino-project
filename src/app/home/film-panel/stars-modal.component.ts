012931dee8ecdb45002d46736afa197298ac1b5f
Lukasz Pietrzykowski <lukaszpietrzykowsk@gmail.com>
2023-02-15 15:55:18 +0100

    feat: displaying modal with ratings

--- src/app/home/film-panel/stars-modal.component.ts
+++ src/app/home/film-panel/stars-modal.component.ts
@@ -9,9 +9,25 @@ import { Component } from '@angular/core';
 })
 export class starsModal {
   numberOfStars: Array<number> = [];
+  starValue = 0;
+
+  status: boolean = false;
+
+  setStar(numberOfStar: number) {
+    this.status = !this.status;
+    this.starValue = numberOfStar;
+  }
+
+  sendRating() {
+    if (this.starValue === 0) {
+      return;
+    }
+    console.log('hello');
+    // sendRatingToBackend()
+  }
 
   ngOnInit() {
-    for (let i = 0; i <= 10; i++) {
+    for (let i = 1; i <= 10; i++) {
       this.numberOfStars = [...this.numberOfStars, ...[i]];
     }
   }
