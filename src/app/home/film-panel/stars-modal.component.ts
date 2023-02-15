92b57a1ac04c230080ed199cd2b174efcfcc4f06
Lukasz Pietrzykowski <lukaszpietrzykowsk@gmail.com>
2023-02-15 15:31:00 +0100

    feat: displaying modal

--- src/app/home/film-panel/stars-modal.component.ts
+++ src/app/home/film-panel/stars-modal.component.ts
@@ -1,7 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'stars-modal',
+  standalone: true,
   templateUrl: 'stars-modal.component.html',
+  imports: [CommonModule],
 })
-export class starsModal {}
+export class starsModal {
+  numberOfStars: Array<number> = [];
+
+  ngOnInit() {
+    for (let i = 0; i <= 10; i++) {
+      this.numberOfStars = [...this.numberOfStars, ...[i]];
+    }
+  }
+}
