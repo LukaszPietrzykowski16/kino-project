facbf03caac418b873621d87bfa530875201d6f2
LukaszPietrzykowski16 <lukaszpietrzykowsk@gmail.com>
2023-02-28 16:40:53 +0100

    making service to limit films

--- src/app/admin/admin-screenings/services/exact-screening.service.ts
+++ src/app/admin/admin-screenings/services/exact-screening.service.ts
@@ -1,17 +1,36 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 import { Screening } from 'src/app/home/film-panel/film-panel.component';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ExactScreeningService {
-  http = inject(HttpClient);
+  private http = inject(HttpClient);
+  private screeningInDay$$ = new BehaviorSubject<Number[]>([]);
+
+  get getScreeningInDay$() {
+    return this.screeningInDay$$.asObservable();
+  }
+
+  date = '';
 
   getScreening(date: string) {
-    console.log(date);
+    this.date = date;
+    this.fetchScreening().subscribe((screening) => {
+      screening.map((screen) => {
+        this.screeningInDay$$.next([
+          ...this.screeningInDay$$.getValue(),
+          ...[screen.filmId],
+        ]);
+      });
+    });
+  }
+
+  fetchScreening() {
     return this.http.get<Screening[]>(
-      `http://localhost:3000/screening?date=${date}`
+      `http://localhost:3000/screening?date=${this.date}`
     );
   }
 
