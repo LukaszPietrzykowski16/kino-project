d9838ff17bc1e839664f51b3a857623ee84ac7e3
Lukasz Pietrzykowski <lukaszpietrzykowsk@gmail.com>
2023-03-02 12:47:45 +0100

    feat: want to watch film improved

--- src/app/domains/want-watch/want-watch-film/want-watch-film.component.ts
+++ src/app/domains/want-watch/want-watch-film/want-watch-film.component.ts
@@ -2,7 +2,6 @@ import { Component, inject, Input } from '@angular/core';
 import { Film } from 'src/app/home/film-panel/film-panel.component';
 import { WantWatchService } from 'src/app/home/film-panel/services/want-watch.service';
 import { IconsModule } from 'src/app/icons/icons.module';
-import { FilmService } from '../film/film.service';
 
 @Component({
   selector: 'app-want-watch-film',
