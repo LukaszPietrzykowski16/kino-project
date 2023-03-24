954943f6a850ab0aef4c571d31e57d625fc8a4b9
LukaszPietrzykowski16 <lukaszpietrzykowsk@gmail.com>
2023-03-24 15:20:03 +0100

    feat: adding new selectors

--- src/app/domains/reservation-hall/store/hall.effects.ts
+++ src/app/domains/reservation-hall/store/hall.effects.ts
@@ -1,17 +1,28 @@
 import { inject, Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { select, Store } from '@ngrx/store';
-import { switchMap, map, tap, of, withLatestFrom } from 'rxjs';
+import {
+  switchMap,
+  map,
+  tap,
+  of,
+  withLatestFrom,
+  combineLatest,
+  filter,
+} from 'rxjs';
 import { HallService } from '../hall.service';
 import { addCinemaHallFromApi, addOrderAction } from './hall.action';
 import { Order, OrderState } from './hall.interface';
+import { selectorOrder } from './hall.selector';
 
 @Injectable()
 export class HallEffects {
   private hallService = inject(HallService);
   private actions$ = inject(Actions);
 
-  private orderStore = inject<Store<OrderState>>(Store);
+  private store = inject<Store<OrderState>>(Store);
+
+  test = this.store.pipe(select(selectorOrder));
 
   hall$ = createEffect(() =>
     this.actions$.pipe(
@@ -26,12 +37,41 @@ export class HallEffects {
     )
   );
 
-  order$ = createEffect(() =>
+  // order$ = createEffect(() =>
+  //   this.actions$.pipe(
+  //     ofType(addOrderAction.decideOrder),
+  //     combineLatest(
+  //       this.orderStore.pipe(select((state) => state.order.position)),
+  //       this.orderStore.pipe(select((state) => state.order.position)),
+  //     )
+  //     filter(([action, val1, val2]) => )
+  //   )
+  // );
+
+  /*
+ combineLatest(
+        this.store.pipe(select((state) => state)),
+        this.store.pipe(select((state) => state))
+      )
+
+  */
+
+  // compareValues$ = createEffect(() =>
+  //   this.actions$.pipe(
+  //     ofType(addOrderAction.decideOrder),
+  //     switchMap(() => {
+  //       return this.addOrder$;
+  //     })
+  //   )
+  // );
+
+  myEffect$ = createEffect(() =>
     this.actions$.pipe(
       ofType(addOrderAction.decideOrder),
-      switchMap((result) => {
-        const position = result.order[0].position;
-
+      withLatestFrom(this.store.select((state) => state.order)),
+      switchMap(([action, myValue]) => {
+        combineLatest;
+        console.log(action.order, myValue.position);
         return this.addOrder$;
       })
     )
